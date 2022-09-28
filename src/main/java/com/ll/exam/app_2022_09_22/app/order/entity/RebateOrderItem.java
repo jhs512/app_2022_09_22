69b494acd7be537af5133d8962172a30f185f636
jhs512 <jangka512@gmail.com>
2022-09-28 22:34:14 +0900

    실험

--- src/main/java/com/ll/exam/app_2022_09_22/app/order/entity/RebateOrderItem.java
+++ src/main/java/com/ll/exam/app_2022_09_22/app/order/entity/RebateOrderItem.java
@@ -1,6 +1,8 @@
 package com.ll.exam.app_2022_09_22.app.order.entity;
 
 import com.ll.exam.app_2022_09_22.app.base.entity.BaseEntity;
+import com.ll.exam.app_2022_09_22.app.member.entity.Member;
+import com.ll.exam.app_2022_09_22.app.product.entity.Product;
 import com.ll.exam.app_2022_09_22.app.product.entity.ProductOption;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,9 +10,7 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 import static javax.persistence.FetchType.LAZY;
 
@@ -31,6 +31,27 @@ public class RebateOrderItem extends BaseEntity {
 
     @ManyToOne(fetch = LAZY)
     private ProductOption productOption;
+
+    @Embedded
+    private RebateOrderItem.EmbMember embMember;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "isSoldOut", column = @Column(name = "product_option_is_soldout")),
+            @AttributeOverride(name = "price", column = @Column(name = "product_option_price")),
+            @AttributeOverride(name = "salePrice", column = @Column(name = "product_option_sale_price")),
+            @AttributeOverride(name = "wholesalePrice", column = @Column(name = "product_option_wholesale_price"))
+    })
+    private RebateOrderItem.EmbProductOption embProductOption;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "isSoldOut", column = @Column(name = "product_is_soldout")),
+            @AttributeOverride(name = "price", column = @Column(name = "product_price")),
+            @AttributeOverride(name = "salePrice", column = @Column(name = "product_sale_price")),
+            @AttributeOverride(name = "wholesalePrice", column = @Column(name = "product_wholesale_price"))
+    })
+    private EmbProduct embProduct;
 
     private int quantity;
 
@@ -57,6 +78,12 @@ public class RebateOrderItem extends BaseEntity {
         refundPrice = orderItem.getRefundPrice();
         refundQuantity = orderItem.getRefundQuantity();
         isPaid = orderItem.isPaid();
+
+        Member member = orderItem.getOrder().getMember();
+
+        embMember = new EmbMember(member);
+        embProductOption = new EmbProductOption(productOption);
+        embProduct = new EmbProduct(productOption.getProduct());
     }
 
     public RebateOrderItem(ProductOption productOption, int quantity) {
@@ -82,5 +109,67 @@ public class RebateOrderItem extends BaseEntity {
 
         this.refundQuantity = quantity;
         this.refundPrice = payPrice;
+    }
+
+    @Embeddable
+    @NoArgsConstructor
+    public static class EmbMember {
+        @OneToOne(fetch = LAZY)
+        private Member member;
+        private String username;
+        private String email;
+
+        public EmbMember(Member member) {
+            this.member = member;
+            username = member.getUsername();
+            email = member.getEmail();
+        }
+    }
+
+    @Embeddable
+    @NoArgsConstructor
+    public static class EmbProductOption {
+        private String color;
+        private String size;
+        private String displayColor;
+        private String displaySize;
+        private int price;
+        private int salePrice;
+        private int wholesalePrice;
+        private boolean isSoldOut;
+
+        public EmbProductOption(ProductOption productOption) {
+            color = productOption.getColor();
+            size = productOption.getSize();
+            displayColor = productOption.getDisplayColor();
+            displaySize = productOption.getDisplaySize();
+            price = productOption.getPrice();
+            salePrice = productOption.getSalePrice();
+            wholesalePrice = productOption.getWholesalePrice();
+            isSoldOut = productOption.isSoldOut();
+        }
+    }
+
+    @Embeddable
+    @NoArgsConstructor
+    public static class EmbProduct {
+
+        private int salePrice;
+        private int price;
+        private int wholesalePrice;
+
+        private String name;
+        private String makerShopName;
+
+        private boolean isSoldOut;
+
+        public EmbProduct(Product product) {
+            salePrice = product.getSalePrice();
+            price = product.getPrice();
+            wholesalePrice = product.getWholesalePrice();
+            name = product.getName();
+            makerShopName = product.getMakerShopName();
+            isSoldOut = product.isSoldOut();
+        }
     }
 }
