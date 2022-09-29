a28b9c36a4dde7fcdc5a108eb3a10c858600ad02
jhs512 <jangka512@gmail.com>
2022-09-29 14:19:00 +0900

    22 09 29, ken 10481, 54강, 날짜 문자열을 통해서 LocalDateTime 객체를 만들어주는 함수 구현

--- src/test/java/com/ll/exam/app_2022_09_22/UtilTests.java
+++ src/test/java/com/ll/exam/app_2022_09_22/UtilTests.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtilTests {
@@ -18,5 +22,15 @@ public class UtilTests {
         int endDayOf__2022_08 = Util.date.getEndDayOf(2022, 8);
 
         assertThat(endDayOf__2022_08).isEqualTo(31);
+    }
+
+    @Test
+    @DisplayName("문자열로 LocalDateTime 객체 만들기")
+    void t2() {
+        LocalDateTime localDateTime1 = Util.date.parse("yyyy-MM-dd HH:mm:ss.SSSSSS", "2022-09-01 23:59:59.999999");
+        assertThat(localDateTime1.toString().length()).isEqualTo(26);
+
+        LocalDateTime localDateTime2 = Util.date.parse("yyyy-MM-dd HH:mm:ss", "2022-09-01 23:59:59");
+        assertThat(localDateTime2.toString().length()).isEqualTo(19);
     }
 }
